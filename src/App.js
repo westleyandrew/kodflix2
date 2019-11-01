a46d83a553cad55ad6cdd8f23646b0aa7705bfbe
Westley Andrew <westleyandrew@hotmail.com>
2019-11-01 08:51:44 +0000

    replace titles with covers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
-import matrix from './images/matrix.jpeg';
-import big from './images/big.jpeg';
-import captain from './images/captain.jpeg';
-import deli from './images/deli.jpeg';
-import guardians from './images/guardians.jpeg';
-import res from './images/res.jpeg';
+import matrix from './images/matrix.jpg';
+import big from './images/big.jpg';
+import captain from './images/captain.jpg';
+import deli from './images/deli.jpg';
+import guardians from './images/guardians.jpg';
+import res from './images/res.jpg';
 
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <img src={matrix} alt='matrix' />
+        <br />
+        <div className='container'>
+          <div className='item'>
+            <img src={big} alt='big' />
+          </div>
+          <div className='item'>
+            <img src={captain} alt='captain' />
+          </div>
+          <div className='item'>
+            <img src={deli} alt='deli' />
+          </div>
+        </div>
+        <div className='container'>
+          <div className='item'>
+            <img src={guardians} alt='guardians' />
+          </div>
+          <div className='item'>
+            <img src={matrix} alt='matrix' />
+          </div>
+          <div className='item'>
+            <img src={res} alt='res' />
+          </div>
+        </div>
       </div>
     );
   }
