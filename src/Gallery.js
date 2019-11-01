42a06bc4f95f5902f74ad8d955cb59b14e8aef51
Westley Andrew <westleyandrew@hotmail.com>
2019-11-01 14:48:54 +0000

    improve the gallery layout

--- src/Gallery.js
+++ src/Gallery.js
@@ -8,18 +8,16 @@ import guardians from './images/guardians.jpg';
 import res from './images/res.jpg';
 
 export default function Gallery() {
-    return (
-      <div>
-        <div className='container'>
-          <Show id='lebowski' name='The Big Lebowski' img={big} />
-          <Show id='capt' name='Captain America' img={captain} />
-          <Show id='deli' name='Delicatessen' img={deli} />
-        </div>
-        <div className='container'>
-          <Show id='guardians' name='Guardians of the Galaxy' img={guardians} />
-          <Show id='matrix' name='The Matrix' img={matrix} />
-          <Show id='reservoir' name='Reservoir Dogs' img={res} />
-        </div>
+  return (
+    <div>
+      <div className='container'>
+        <Show id='lebowski' name='The Big Lebowski' img={big} />
+        <Show id='capt' name='Captain America' img={captain} />
+        <Show id='deli' name='Delicatessen' img={deli} />
+        <Show id='guardians' name='Guardians of the Galaxy' img={guardians} />
+        <Show id='matrix' name='The Matrix' img={matrix} />
+        <Show id='reservoir' name='Reservoir Dogs' img={res} />
       </div>
-    )
-  }
+    </div>
+  )
+}
