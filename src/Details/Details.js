8c57ab67c1ef2835bb309faf88d6e18e8d1ea8cb
Westley Andrew <westleyandrew@hotmail.com>
2019-11-05 17:32:20 +0000

    add synopsis and cover

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import getGallery from './gallery-get';
+import getGallery from '../gallery-get';
+import './Details.css';
 
 export default class extends Component {
 
@@ -23,8 +24,15 @@ export default class extends Component {
       return <Redirect to='not-found' />;
     } else {
       return (
-        <div>
+        <div className='Details'>
           <h1>{this.state.show.name}</h1>
+          <div className='content'> 
+            <div className='text'>{this.state.show.synopsis}</div>
+            <img
+              className='image'
+              src={this.state.show.img}
+              alt={this.state.show.name} />
+          </div>
           <Link to='/'>Back to home page</Link>
         </div>
       );
--- src/gallery-get.js
+++ src/gallery-get.js
@@ -7,7 +7,7 @@ import res from './images/res.jpg';
 
 export default function getGallery() {
     return [
-      { id: 'lebowski', name: 'The Big Lebowski', img: big},
+      { id: 'lebowski', name: 'The Big Lebowski', img: big, synopsis: 'Jeff \`The Dude\' Leboswki is mistaken for Jeffrey Lebowski, who is The Big Lebowski. Which explains why he\'s roughed up and has his precious rug peed on. In search of recompense, The Dude tracks down his namesake, who offers him a job. His wife has been kidnapped and he needs a reliable bagman. Aided and hindered by his pals Walter Sobchak, a Vietnam vet, and Donny, master of stupidity.'},
       { id: 'capt', name: 'Captain America', img: captain},
       { id: 'deli', name: 'Delicatessen', img: deli},
       { id: 'guardians', name: 'Guardians of the Galaxy', img: guardians},
